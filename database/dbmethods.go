8bf79e7452abdc126977aee485378ee44f0843e2
toke <touko@testausserveri.fi>
2023-06-11 14:35:26 +0300

    feat: introduce show amount argument

--- database/dbmethods.go
+++ database/dbmethods.go
@@ -5,47 +5,59 @@ import (
 )
 
 func GetTodos() ([]todos.Todo, error) {
-    ensureDb()
-    rows, err := db.Query(`SELECT * FROM todos`)
-    if err != nil {
-        return nil, err
-    }
-    defer rows.Close()
+	ensureDb()
+	rows, err := db.Query(`SELECT * FROM todos`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
-    var tasks todos.Todos
-    for rows.Next() {
-        var todo todos.Todo
-        if err := rows.Scan(
-            &todo.ID, &todo.Task, &todo.Done, &todo.Date, &todo.Deadline,
-        ); err != nil {
-            return tasks.SortByDone(), err
-        }
-        tasks = append(tasks, todo)
-    }
+	var tasks todos.Todos
+	for rows.Next() {
+		var todo todos.Todo
+		if err := rows.Scan(
+			&todo.ID, &todo.Task, &todo.Done, &todo.Date, &todo.Deadline); err != nil {
+			return tasks.SortByDone(), err
+		}
+		tasks = append(tasks, todo)
+	}
 
-    return tasks.SortByDone(), nil
+	return tasks.SortByDone(), nil
+}
+
+func GetTodosAmount() (int32, error) {
+	ensureDb()
+	row := db.QueryRow("SELECT COUNT(*) FROM todos")
+	if err := row.Err(); err != nil {
+		return 0, err
+	}
+
+	var amount int32
+	err := row.Scan(&amount)
+	return amount, err
 }
 
 func NewTodo(task string, deadline int, date int) (int64, error) {
-    ensureDb()
+	ensureDb()
 
-    x, err := db.Exec(`INSERT INTO todos(task, done, deadline, date) VALUES (?, ?, ?, ?)`, 
-        task, false, deadline, date)
-    if err != nil { return 0, err }
+	x, err := db.Exec(`INSERT INTO todos(task, done, deadline, date) VALUES (?, ?, ?, ?)`,
+		task, false, deadline, date)
+	if err != nil {
+		return 0, err
+	}
 
-    insertId, err := x.LastInsertId()
-    return insertId, err
+	insertId, err := x.LastInsertId()
+	return insertId, err
 }
 
 func UpdateDoneById(id int, done bool) error {
-    ensureDb()
-    _, err := db.Exec(`UPDATE todos SET done = ? WHERE id = ?`, done, id)
-    return err
+	ensureDb()
+	_, err := db.Exec(`UPDATE todos SET done = ? WHERE id = ?`, done, id)
+	return err
 }
 
 func RemoveTodoById(id int) error {
-    ensureDb()
-    _, err := db.Exec(`DELETE FROM todos WHERE id = ?`, id)
-    return err
+	ensureDb()
+	_, err := db.Exec(`DELETE FROM todos WHERE id = ?`, id)
+	return err
 }
-
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	ui "github.com/gizak/termui/v3"
 	"github.com/gizak/termui/v3/widgets"
 	"github.com/romeq/godo/database"
@@ -9,164 +12,208 @@ import (
 )
 
 func renderScreen(widget ui.Drawable) {
-    screen_width, screen_height := ui.TerminalDimensions()
-    if screen_width > 120 && screen_height > 35 {
-        widget.SetRect(screen_width/4, screen_height/4, screen_width/4*3, screen_height/4*3)
-    } else if screen_width > 80 && screen_height > 35 {
-        widget.SetRect(8, screen_height / 4, screen_width-8, screen_height / 4*3)
-    } else if screen_width < 80 && screen_height > 35 {
-        widget.SetRect(4, screen_height/4, screen_width-4, screen_height/4*3)
-    } else if screen_width > 80 && screen_height < 35 {
-        widget.SetRect(screen_width/10*2, 2, screen_width/10*8, screen_height-2)
-    } else {
-        widget.SetRect(4, 2, screen_width-4, screen_height-2)
-    }
+	screen_width, screen_height := ui.TerminalDimensions()
+	if screen_width > 120 && screen_height > 35 {
+		widget.SetRect(screen_width/4, screen_height/4, screen_width/4*3, screen_height/4*3)
+	} else if screen_width > 80 && screen_height > 35 {
+		widget.SetRect(8, screen_height/4, screen_width-8, screen_height/4*3)
+	} else if screen_width < 80 && screen_height > 35 {
+		widget.SetRect(4, screen_height/4, screen_width-4, screen_height/4*3)
+	} else if screen_width > 80 && screen_height < 35 {
+		widget.SetRect(screen_width/10*2, 2, screen_width/10*8, screen_height-2)
+	} else {
+		widget.SetRect(4, 2, screen_width-4, screen_height-2)
+	}
 
-    ui.Render(widget)
+	ui.Render(widget)
 }
 
+func getArg(index int) string {
+	if len(os.Args) > index {
+		return os.Args[index]
+	}
+	return ""
+}
 
 func main() {
-    utils.Check(ui.Init())
-    defer ui.Close()
+	err := database.Init()
+	utils.Check(err)
 
-    err := database.Init()
-    utils.Check(err)
-    
-    uicolor := ui.ColorMagenta
+	if getArg(1) == "--show-amount" {
+		amount, err := database.GetTodosAmount()
+		utils.Check(err)
+		if getArg(2) == "" {
+			fmt.Printf("You have %d To-Dos left \n", amount)
+		} else if getArg(2) == "--output-raw" {
+			fmt.Println(amount)
+		} else {
+			fmt.Println("err: unknown type")
+		}
+		return
+	} else if getArg(1) != "" {
+		fmt.Println("./godo [--show-amount [--output-raw]]")
+	}
 
-    todoListWidget := widgets.NewList()
-    todoListWidget.Title = "Loading your To-Dos..."
-    todoListWidget.Rows = []string{}
-    todoListWidget.SelectedRowStyle = ui.NewStyle(uicolor)
-    todoListWidget.TitleStyle = ui.NewStyle(uicolor)
-    todoListWidget.BorderStyle = ui.NewStyle(ui.ColorBlack)
-    renderScreen(todoListWidget)
+	utils.Check(ui.Init())
+	defer ui.Close()
 
-    screen_width, _ := ui.TerminalDimensions()
-    input_prefix := " > "
-    inputWidget := widgets.NewParagraph()
-    inputWidget.SetRect(2, 1, screen_width-2, 4)
-    inputWidget.Text = input_prefix
-    inputWidget.Title = "Add a To-Do"
-    inputWidget.BorderStyle = todoListWidget.BorderStyle
+	uicolor := ui.ColorMagenta
 
-    // load tasks in background
-    var tasks todos.Todos
-    go func() {
-        db_tasks, err := database.GetTodos()
-        utils.Check(err)
+	todoListWidget := widgets.NewList()
+	todoListWidget.Title = "Loading your To-Dos..."
+	todoListWidget.Rows = []string{}
+	todoListWidget.SelectedRowStyle = ui.NewStyle(uicolor)
+	todoListWidget.TitleStyle = ui.NewStyle(uicolor)
+	todoListWidget.BorderStyle = ui.NewStyle(ui.ColorBlack)
+	renderScreen(todoListWidget)
 
-        for _, todo := range db_tasks {
-            tasks = append(tasks, todo)
-            todoListWidget.Rows = append(todoListWidget.Rows, todo.Display())
-        }
+	screen_width, _ := ui.TerminalDimensions()
+	input_prefix := " > "
+	inputWidget := widgets.NewParagraph()
+	inputWidget.SetRect(2, 1, screen_width-2, 4)
+	inputWidget.Text = input_prefix
+	inputWidget.Title = "Add a To-Do"
+	inputWidget.BorderStyle = todoListWidget.BorderStyle
 
-        todoListWidget.Title = "To-Dos"
-        renderScreen(todoListWidget)
-    }()
+	// load tasks in background
+	var tasks todos.Todos
+	go func() {
+		db_tasks, err := database.GetTodos()
+		utils.Check(err)
 
-    events := ui.PollEvents()
-    inputLoop:for {
-        e := <-events
-        switch e.ID {
-        case "q", "<C-c>", "<C-q>":
-            break inputLoop
-        case "j", "<Down>":
-            if len(todoListWidget.Rows) == 0 { break }
-            todoListWidget.ScrollDown()
-        case "k", "<Up>":
-            if len(todoListWidget.Rows) == 0 { break }
-            todoListWidget.ScrollUp() 
-        case "<C-d>":
-            if len(todoListWidget.Rows) == 0 { break }
-            todoListWidget.ScrollHalfPageDown()
-        case "<C-u>":
-            if len(todoListWidget.Rows) == 0 { break }
-            todoListWidget.ScrollHalfPageUp()
-        case "<C-f>":
-            if len(todoListWidget.Rows) == 0 { break }
-            todoListWidget.ScrollPageDown() 
-        case "<C-b>":
-            if len(todoListWidget.Rows) == 0 { break }
-            todoListWidget.ScrollPageUp()
-        case "g", "<Home>":
-            if len(todoListWidget.Rows) == 0 { break }
-            todoListWidget.ScrollTop()
-        case "G", "<End>":
-            if len(todoListWidget.Rows) == 0 { break }
-            todoListWidget.ScrollBottom() 
-        case "d":
-            if len(todoListWidget.Rows) == 0 { break }
-            // Get current task from tasks array and delete todo from database
-            currentTask := &tasks[todoListWidget.SelectedRow]
-            utils.Check(database.RemoveTodoById(currentTask.ID))
+		for _, todo := range db_tasks {
+			tasks = append(tasks, todo)
+			todoListWidget.Rows = append(todoListWidget.Rows, todo.Display())
+		}
 
-            // Remove task from both lists
-            listRows := todoListWidget.Rows
-            selectedRow := todoListWidget.SelectedRow
-            tasks = append(tasks[:selectedRow], tasks[selectedRow+1:]...)
-            todoListWidget.Rows = append(listRows[:selectedRow], listRows[selectedRow+1:]...)
+		todoListWidget.Title = "To-Dos"
+		renderScreen(todoListWidget)
+	}()
 
-            // Scroll up if deleted todo was not last one 
-            if len(todoListWidget.Rows) == selectedRow { 
-                todoListWidget.ScrollUp() 
-            }
+	events := ui.PollEvents()
+inputLoop:
+	for {
+		e := <-events
+		switch e.ID {
+		case "q", "<C-c>", "<C-q>":
+			break inputLoop
+		case "j", "<Down>":
+			if len(todoListWidget.Rows) == 0 {
+				break
+			}
+			todoListWidget.ScrollDown()
+		case "k", "<Up>":
+			if len(todoListWidget.Rows) == 0 {
+				break
+			}
+			todoListWidget.ScrollUp()
+		case "<C-d>":
+			if len(todoListWidget.Rows) == 0 {
+				break
+			}
+			todoListWidget.ScrollHalfPageDown()
+		case "<C-u>":
+			if len(todoListWidget.Rows) == 0 {
+				break
+			}
+			todoListWidget.ScrollHalfPageUp()
+		case "<C-f>":
+			if len(todoListWidget.Rows) == 0 {
+				break
+			}
+			todoListWidget.ScrollPageDown()
+		case "<C-b>":
+			if len(todoListWidget.Rows) == 0 {
+				break
+			}
+			todoListWidget.ScrollPageUp()
+		case "g", "<Home>":
+			if len(todoListWidget.Rows) == 0 {
+				break
+			}
+			todoListWidget.ScrollTop()
+		case "G", "<End>":
+			if len(todoListWidget.Rows) == 0 {
+				break
+			}
+			todoListWidget.ScrollBottom()
+		case "d":
+			if len(todoListWidget.Rows) == 0 {
+				break
+			}
+			// Get current task from tasks array and delete todo from database
+			currentTask := &tasks[todoListWidget.SelectedRow]
+			utils.Check(database.RemoveTodoById(currentTask.ID))
 
-        case "<Space>": // Toggle done
-            if len(todoListWidget.Rows) == 0 { break }
+			// Remove task from both lists
+			listRows := todoListWidget.Rows
+			selectedRow := todoListWidget.SelectedRow
+			tasks = append(tasks[:selectedRow], tasks[selectedRow+1:]...)
+			todoListWidget.Rows = append(listRows[:selectedRow], listRows[selectedRow+1:]...)
 
-            // Get current task and toggle it's done property
-            currentTask := &tasks[todoListWidget.SelectedRow]
-            currentTask.Done = !currentTask.Done
+			// Scroll up if deleted todo was not last one
+			if len(todoListWidget.Rows) == selectedRow {
+				todoListWidget.ScrollUp()
+			}
 
-            // Update the todo in database and screen
-            todoListWidget.Rows[todoListWidget.SelectedRow] = currentTask.Display()
-            utils.Check(database.UpdateDoneById(currentTask.ID, currentTask.Done))
+		case "<Space>": // Toggle done
+			if len(todoListWidget.Rows) == 0 {
+				break
+			}
 
-        case "<Enter>": // Create a new todo
-            var input_text string
-            renderScreen(inputWidget)
-            input_evs := ui.PollEvents()
-            newtask_prompt_loop:for {
-                ev := <-input_evs
-                switch ev.ID {
-                case "<Enter>":
-                    if len(input_text) > 0 {
-                        todo_id, err := database.NewTodo(input_text, 0, 0)
-                        utils.Check(err)
-                        todo := todos.New(int(todo_id), input_text, 0, 0)
-                        tasks = append(tasks, todo)
-                        todoListWidget.Rows = append(todoListWidget.Rows, todo.Display())
-                        inputWidget.Text = input_prefix
-                    }
-                    break newtask_prompt_loop
-                case "<Backspace>":
-                    if len(input_text) > 0 {
-                        input_text = input_text[:len(input_text)-1]
-                    }
-                    inputWidget.Text = input_prefix + input_text
-                    renderScreen(inputWidget)
-                case "<Escape>", "<C-c>":
-                    inputWidget.Text = input_prefix
-                    break newtask_prompt_loop
-                default:
-                    x := ev.ID
-                    if x == "<Space>" {
-                        x = " "
-                    }
-                    if len(x) != 1 { break }
+			// Get current task and toggle it's done property
+			currentTask := &tasks[todoListWidget.SelectedRow]
+			currentTask.Done = !currentTask.Done
 
-                    input_text += x
-                    inputWidget.Text = input_prefix + input_text
-                    renderScreen(inputWidget)
-                }
-            }
-        case "<Resize>":
-            ui.Clear()
-        }
-        renderScreen(todoListWidget)
+			// Update the todo in database and screen
+			todoListWidget.Rows[todoListWidget.SelectedRow] = currentTask.Display()
+			utils.Check(database.UpdateDoneById(currentTask.ID, currentTask.Done))
 
-    }
+		case "<Enter>": // Create a new todo
+			var input_text string
+			renderScreen(inputWidget)
+			input_evs := ui.PollEvents()
+		newtask_prompt_loop:
+			for {
+				ev := <-input_evs
+				switch ev.ID {
+				case "<Enter>":
+					if len(input_text) > 0 {
+						todo_id, err := database.NewTodo(input_text, 0, 0)
+						utils.Check(err)
+						todo := todos.New(int(todo_id), input_text, 0, 0)
+						tasks = append(tasks, todo)
+						todoListWidget.Rows = append(todoListWidget.Rows, todo.Display())
+						inputWidget.Text = input_prefix
+					}
+					break newtask_prompt_loop
+				case "<Backspace>":
+					if len(input_text) > 0 {
+						input_text = input_text[:len(input_text)-1]
+					}
+					inputWidget.Text = input_prefix + input_text
+					renderScreen(inputWidget)
+				case "<Escape>", "<C-c>":
+					inputWidget.Text = input_prefix
+					break newtask_prompt_loop
+				default:
+					x := ev.ID
+					if x == "<Space>" {
+						x = " "
+					}
+					if len(x) != 1 {
+						break
+					}
 
+					input_text += x
+					inputWidget.Text = input_prefix + input_text
+					renderScreen(inputWidget)
+				}
+			}
+		case "<Resize>":
+			ui.Clear()
+		}
+		renderScreen(todoListWidget)
+
+	}
 }
